fe5c3788417b3b379e434aa02ade354d093d807b
rkugot <stream13k@gmail.com>
2018-01-09 11:04:10 +0300

    Add region controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Spree::Core::Engine.add_routes do
   post '/currency/set', to: 'currency#set', defaults: { format: :json }, as: :set_currency
+  post '/region/set', to: 'region#set', defaults: { format: :json }, as: :set_region
 
   namespace :admin do
     resources :products do
