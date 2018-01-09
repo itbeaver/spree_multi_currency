7767f8819cc23e8847b38971897e02c668ec3e4e
rkugot <stream13k@gmail.com>
2018-01-09 11:04:33 +0300

    Add region to currency method

--- lib/spree/core/currency_helpers.rb
+++ lib/spree/core/currency_helpers.rb
@@ -7,5 +7,21 @@ module Spree
     def supported_currencies
       Spree::Config[:supported_currencies].split(',').map { |code| ::Money::Currency.find(code.strip) }
     end
+
+    def supported_regions
+      Spree::Config[:supported_regions].split(',').map { |code| code.strip }
+    end
+
+    def region_to_currency(region)
+      if region == 'USA'
+        'USD'
+      elsif region == 'UK'
+        'GBP'
+      elsif region == 'EU'
+        'EUR'
+      else
+        'USD'
+      end
+    end
   end
 end
