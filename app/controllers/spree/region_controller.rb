52eda50859235b58836740c51d18b3dceb55632c
rkugot <stream13k@gmail.com>
2018-01-18 09:26:13 +0300

    Fix region controller

--- app/controllers/spree/region_controller.rb
+++ app/controllers/spree/region_controller.rb
@@ -2,10 +2,20 @@ module Spree
   class RegionController < StoreController
     def set
       @currency = supported_currencies.find { |currency| currency.iso_code == region_to_currency(params[:region]) }
-      # Make sure that we update the current order, so the currency change is reflected.
-      current_order.update_attributes!(currency: @currency.iso_code) if current_order
-      session[:region] = params[:region] if Spree::Config[:allow_currency_change]
-      session[:currency] = region_to_currency(params[:region])
+
+      if current_order.present?
+        # Make sure that we update the current order, so the currency change is reflected.
+        current_order.update_attributes!(currency: @currency.iso_code)
+        # Make sure that we update taxes
+        # https://github.com/spree/spree/blob/bc0c3679fb137394c59cfe5651ffe561ee2e4722/core/app/models/spree/order.rb#L285
+        current_order.create_tax_charge!
+        current_order.update!
+      end
+
+      if Spree::Config[:allow_currency_change]
+        session[:region] = params[:region]
+        session[:currency] = region_to_currency(params[:region])
+      end
       respond_to do |format|
         format.json { render json: !@currency.nil? }
         format.html do
